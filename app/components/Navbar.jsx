4f35cc180de2051bd9eaf31abfa6c3bc9493c74b
Taku.G <taku.gunda05@gmail.com>
2025-08-28 13:41:15 +0200

    Add Navbar and NavLink components with navigation links

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,5 +1,14 @@
 import Link from "next/link";
 import React from "react";
+import NavLink from "./NavLink";
+
+const navLinks = [
+  { href: "#home", title: "Home" },
+  { href: "#about", title: "About" },
+  { href: "#services", title: "Services" },
+  { href: "#portfolio", title: "Portfolio" },
+  { href: "#contact", title: "Contact" },
+];
 
 const Navbar = () => {
   return (
@@ -8,7 +17,15 @@ const Navbar = () => {
         <Link href={"/"} className="text-5xl text-white font-semibold">
           Logo goes here
         </Link>
-        <div className="menu"></div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 lg:p-0 mt-4 md:mt-0 flex-col md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.href} title={link.title} />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
